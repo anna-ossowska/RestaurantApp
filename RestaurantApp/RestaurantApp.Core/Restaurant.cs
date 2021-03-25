43f10cf7f76f7b65cf5c97fc9f1c34d499bec5de
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-25 16:09:46 +0100

    "Adding validation checks"

--- RestaurantApp/RestaurantApp.Core/Restaurant.cs
+++ RestaurantApp/RestaurantApp.Core/Restaurant.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 // Represents classes and algorithms that are the core of application
 
@@ -9,8 +10,13 @@ namespace RestaurantApp.Core
     public class Restaurant
     {
         public int Id { get; set; }
+
+        [Required, StringLength(80)]
         public string Name { get; set; }
+
+        [Required, StringLength(255)]
         public string Location { get; set; }
+
         public CuisineType Cuisine { get; set; }
     }
 }
