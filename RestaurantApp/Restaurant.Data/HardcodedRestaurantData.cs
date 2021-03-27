36001e5aed59735e24651ae2ce757b10ae3138e0
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-27 20:59:13 +0100

    Implementing the Delete functionality

---
+++ RestaurantApp/Restaurant.Data/HardcodedRestaurantData.cs
@@ -0,0 +1,73 @@
+﻿using RestaurantApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantApp.Data
+{
+    public class HardcodedRestaurantData : IRestaurantData
+    {
+        public List<Restaurant> restaurants;
+
+        public HardcodedRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Flavoroso", Location = "Stavanger", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Green Curry", Location = "Bergen", Cuisine = CuisineType.Indian },
+                new Restaurant { Id = 3, Name = "Salty Squid", Location = "Trondheim", Cuisine = CuisineType.Mexican},
+                new Restaurant { Id = 4, Name = "El Pirata Porch", Location = "Sola", Cuisine = CuisineType.None }
+            };
+            
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var resturant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+
+            if (resturant != null)
+            {
+                resturant.Name = updatedRestaurant.Name;
+                resturant.Location = updatedRestaurant.Location;
+                resturant.Cuisine = updatedRestaurant.Cuisine;             
+            }
+
+            return resturant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public Restaurant Create(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }        
+            return restaurant;
+        }
+    }
+}
